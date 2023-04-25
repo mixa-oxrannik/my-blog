ad6e0eec3aeb35727bca088c6cfe75c8d0035572
Your Name <you@example.com>
2023-04-25 22:13:43 +0800

    Added the ability to delete a post

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
